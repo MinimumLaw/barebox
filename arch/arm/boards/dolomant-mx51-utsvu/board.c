1ade234d1e1e518ebf79d8c258217aee9f8359a0
Alex A. Mihaylov <minimumlaw@rambler.ru>
2016-07-26 14:59:00 +0300

    UTSVU: remove magic constants, now symbolic names

--- arch/arm/boards/dolomant-mx51-utsvu/board.c
+++ arch/arm/boards/dolomant-mx51-utsvu/board.c
@@ -32,6 +32,7 @@
 #include <notifier.h>
 #include <spi/spi.h>
 #include <mfd/mc13xxx.h>
+#include <mfd/mc13892.h>
 #include <io.h>
 #include <asm/mmu.h>
 #include <mach/imx5.h>
@@ -50,44 +51,55 @@ static void babbage_power_init(struct mc13xxx *mc13xxx)
 
 	/* Write needed to Power Gate 2 register */
 	mc13xxx_reg_read(mc13xxx, MC13892_REG_POWER_MISC, &val);
-	val &= ~0x10000;
+	val &= ~MC13892_POWER_MISC_PWGT2SPIEN;
 	mc13xxx_reg_write(mc13xxx, MC13892_REG_POWER_MISC, val);
 
 	/* Write needed to update Charger 0 */
-	mc13xxx_reg_write(mc13xxx, MC13892_REG_CHARGE, 0x0023807F);
+	mc13xxx_reg_write(mc13xxx, MC13892_REG_CHARGE,
+		MC13782_CHARGE_VCHRG0 |
+		MC13782_CHARGE_VCHRG1 |
+		MC13782_CHARGE_VCHRG2 |
+		MC13782_CHARGE_ICHRG0 |
+		MC13782_CHARGE_ICHRG1 |
+		MC13782_CHARGE_ICHRG2 |
+		MC13782_CHARGE_ICHRG3 |
+		MC13782_CHARGE_PLIM0 |
+		MC13782_CHARGE_PLIM1 |
+		MC13782_CHARGE_PLIMDIS |
+		MC13782_CHARGE_CHGAUTOB );
 
 	/* power up the system first */
-	mc13xxx_reg_write(mc13xxx, MC13892_REG_POWER_MISC, 0x00200000);
+	mc13xxx_reg_write(mc13xxx, MC13892_REG_POWER_MISC, MC13892_POWER_MISC_PWUP);
 
 	if (imx_silicon_revision() < IMX_CHIP_REV_3_0) {
 		/* Set core voltage to 1.1V */
 		mc13xxx_reg_read(mc13xxx, MC13892_REG_SW_0, &val);
-		val &= ~0x1f;
-		val |= 0x14;
+		val &= ~MC13892_SWx_SWx_VOLT_MASK;
+		val |= MC13892_SWx_SWx_1_100V;
 		mc13xxx_reg_write(mc13xxx, MC13892_REG_SW_0, val);
 
 		/* Setup VCC (SW2) to 1.25 */
 		mc13xxx_reg_read(mc13xxx, MC13892_REG_SW_1, &val);
-		val &= ~0x1f;
-		val |= 0x1a;
+		val &= ~MC13892_SWx_SWx_VOLT_MASK;
+		val |= MC13892_SWx_SWx_1_250V;
 		mc13xxx_reg_write(mc13xxx, MC13892_REG_SW_1, val);
 
 		/* Setup 1V2_DIG1 (SW3) to 1.25 */
 		mc13xxx_reg_read(mc13xxx, MC13892_REG_SW_2, &val);
-		val &= ~0x1f;
-		val |= 0x1a;
+		val &= ~MC13892_SWx_SWx_VOLT_MASK;
+		val |= MC13892_SWx_SWx_1_250V;
 		mc13xxx_reg_write(mc13xxx, MC13892_REG_SW_2, val);
 	} else {
 		/* Setup VCC (SW2) to 1.225 */
 		mc13xxx_reg_read(mc13xxx, MC13892_REG_SW_1, &val);
-		val &= ~0x1f;
-		val |= 0x19;
+		val &= ~MC13892_SWx_SWx_VOLT_MASK;
+		val |= MC13892_SWx_SWx_1_225V;
 		mc13xxx_reg_write(mc13xxx, MC13892_REG_SW_1, val);
 
 		/* Setup 1V2_DIG1 (SW3) to 1.2 */
 		mc13xxx_reg_read(mc13xxx, MC13892_REG_SW_2, &val);
-		val &= ~0x1f;
-		val |= 0x18;
+		val &= ~MC13892_SWx_SWx_VOLT_MASK;
+		val |= MC13892_SWx_SWx_1_200V;
 		mc13xxx_reg_write(mc13xxx, MC13892_REG_SW_2, val);
 	}
 
@@ -95,50 +107,72 @@ static void babbage_power_init(struct mc13xxx *mc13xxx)
 		/* Set switchers in PWM mode for Atlas 2.0 and lower */
 		/* Setup the switcher mode for SW1 & SW2*/
 		mc13xxx_reg_read(mc13xxx, MC13892_REG_SW_4, &val);
-		val &= ~0x3c0f;
-		val |= 0x1405;
+		val &=~((MC13892_SWMODE_MASK<<MC13892_SWMODE1_SHIFT) |
+			(MC13892_SWMODE_MASK<<MC13892_SWMODE2_SHIFT));
+		val |= ((MC13892_SWMODE_PWM_PWM<<MC13892_SWMODE1_SHIFT) |
+			(MC13892_SWMODE_PWM_PWM<<MC13892_SWMODE2_SHIFT));
 		mc13xxx_reg_write(mc13xxx, MC13892_REG_SW_4, val);
 
 		/* Setup the switcher mode for SW3 & SW4 */
 		mc13xxx_reg_read(mc13xxx, MC13892_REG_SW_5, &val);
-		val &= ~0xf0f;
-		val |= 0x505;
+		val &=~((MC13892_SWMODE_MASK<<MC13892_SWMODE3_SHIFT) |
+			(MC13892_SWMODE_MASK<<MC13892_SWMODE4_SHIFT));
+		val |= ((MC13892_SWMODE_PWM_PWM<<MC13892_SWMODE3_SHIFT) |
+			(MC13892_SWMODE_PWM_PWM<<MC13892_SWMODE4_SHIFT));
 		mc13xxx_reg_write(mc13xxx, MC13892_REG_SW_5, val);
 	} else {
 		/* Set switchers in Auto in NORMAL mode & STANDBY mode for Atlas 2.0a */
 		/* Setup the switcher mode for SW1 & SW2*/
 		mc13xxx_reg_read(mc13xxx, MC13892_REG_SW_4, &val);
-		val &= ~0x3c0f;
-		val |= 0x2008;
+		val &=~((MC13892_SWMODE_MASK<<MC13892_SWMODE1_SHIFT) |
+			(MC13892_SWMODE_MASK<<MC13892_SWMODE2_SHIFT));
+		val |= ((MC13892_SWMODE_AUTO_AUTO<<MC13892_SWMODE1_SHIFT) |
+			(MC13892_SWMODE_AUTO_AUTO<<MC13892_SWMODE2_SHIFT));
 		mc13xxx_reg_write(mc13xxx, MC13892_REG_SW_4, val);
 
 		/* Setup the switcher mode for SW3 & SW4 */
 		mc13xxx_reg_read(mc13xxx, MC13892_REG_SW_5, &val);
-		val &= ~0xf0f;
-		val |= 0x808;
+		val &=~((MC13892_SWMODE_MASK<<MC13892_SWMODE3_SHIFT) |
+			(MC13892_SWMODE_MASK<<MC13892_SWMODE4_SHIFT));
+		val |= ((MC13892_SWMODE_AUTO_AUTO<<MC13892_SWMODE3_SHIFT) |
+			(MC13892_SWMODE_AUTO_AUTO<<MC13892_SWMODE4_SHIFT));
 		mc13xxx_reg_write(mc13xxx, MC13892_REG_SW_5, val);
 	}
 
-	/* Set VDIG to 1.65V, VGEN3 to 1.8V, VCAM to 2.5V */
+	/* Set VDIG to 1.65V, VGEN3 to 1.8V, VCAM to 2.6V */
 	mc13xxx_reg_read(mc13xxx, MC13892_REG_SETTING_0, &val);
-	val &= ~0x34030;
-	val |= 0x10020;
+	val &=~(MC13892_SETTING_0_VGEN3_MASK |
+		MC13892_SETTING_0_VCAM_MASK);
+	val |= (MC13892_SETTING_0_VDIG_1_65 |
+		MC13892_SETTING_0_VGEN3_1_8 |
+		MC13892_SETTING_0_VCAM_2_6);
 	mc13xxx_reg_write(mc13xxx, MC13892_REG_SETTING_0, val);
 
 	/* Set VVIDEO to 2.775V, VAUDIO to 3V, VSD to 3.15V */
 	mc13xxx_reg_read(mc13xxx, MC13892_REG_SETTING_1, &val);
-	val &= ~0x1FC;
-	val |= 0x1F4;
+	val &=~(MC13892_SETTING_1_VVIDEO_MASK |
+		MC13892_SETTING_1_VAUDIO_MASK |
+		MC13892_SETTING_1_VSD_MASK);
+	val |= (MC13892_SETTING_1_VVIDEO_2_775 |
+		MC13892_SETTING_1_VAUDIO_3_0 |
+		MC13892_SETTING_1_VSD_3_15);
 	mc13xxx_reg_write(mc13xxx, MC13892_REG_SETTING_1, val);
 
 	/* Configure VGEN3 and VCAM regulators to use external PNP */
-	val = 0x208;
+	val  = (MC13892_MODE_1_VGEN3CONFIG |
+		MC13892_MODE_1_VCAMCONFIG );
 	mc13xxx_reg_write(mc13xxx, MC13892_REG_MODE_1, val);
 
 	udelay(200);
 
 	/* Enable VGEN3, VCAM, VAUDIO, VVIDEO, VSD regulators */
-	val = 0x49249;
+	val  = (MC13892_MODE_1_VGEN3EN |
+		MC13892_MODE_1_VGEN3CONFIG |
+		MC13892_MODE_1_VCAMEN |
+		MC13892_MODE_1_VCAMCONFIG |
+		MC13892_MODE_1_VVIDEOEN |
+		MC13892_MODE_1_VAUDIOEN |
+		MC13892_MODE_1_VSDEN);
 	mc13xxx_reg_write(mc13xxx, MC13892_REG_MODE_1, val);
 
 	udelay(200);
@@ -146,7 +180,8 @@ static void babbage_power_init(struct mc13xxx *mc13xxx)
 	pr_info("initialized PMIC\n");
 
 	console_flush();
-	imx51_init_lowlevel(800);
+	/* Industrial grade: max 600MHz, Commericail grade: max 800MHz */
+	imx51_init_lowlevel(600);
 	clock_notifier_call_chain();
 }
 
